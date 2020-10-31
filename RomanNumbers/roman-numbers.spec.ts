d6a0852b0a0259d3407a206663beeaf54aa37bd0
Jonathan Alcántara Jimenez <jalcantara@MBPdeJonathan2.home>
2020-10-31 21:54:39 +0100

    Kata roman numbers

---
+++ RomanNumbers/roman-numbers.spec.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Se precisa crear un programa que dado un número
+ * arábico devuelva su representación en número Romano.
+ */
+
+type RomanNumberTestType = [input: number, expected: string];
+
+describe('RomanNumber Should', () => {
+
+
+});
