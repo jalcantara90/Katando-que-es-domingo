a1d97c615a73c81b29ee4f261cc78ff6913d33a4
Victor <jochinavarro@gmail.com>
2020-11-15 09:50:28 +0100

    not finished solution

--- RomanNumbers/roman-numbers-good-victor.spec.ts
+++ RomanNumbers/roman-numbers-good-victor.spec.ts
@@ -85,21 +85,6 @@ describe('RomanNumerals Should', () => {
   })
 });
 
-//REGLAS
-//El sistema romano, que utiliza los siguientes siete símbolos para la representación(I, V, X, L, C, D y M) y que independientemente de donde estén colocadas las cifras tienen el mismo valor.
-
-//Los símbolos se escriben y leen de izquierda a derecha, de mayor a menor valor.
-
-//Si a la derecha de una cifra romana de escribe otra igual o menor, el valor de ésta se suma a la anterior.
-
-//Si un símbolo está a la izquierda inmediata de otro de mayor valor, se resta al valor del segundo el valor del primero.
-
-//Se permiten a lo sumo tres repeticiones consecutivas del mismo símbolos I, X y C.
-
-//No se permite la repetición de un mismo símbolo.
-
-//La cifra I colocada delante de la V o la X, les resta una unidad; la X, precediendo a la L o a la C, les resta diez unidades y la C, delante de la D o la M, les resta cien unidades.
-
 //diccionario numeros romanos base
 const baseRomanNumbers = {
   1: 'I',
